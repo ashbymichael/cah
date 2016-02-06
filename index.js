5decc7155af54a160a5549322d941ddef50cd448
Michael Ashby <ashby.michael@gmail.com>
2016-02-05 21:27:38 -0800

    add join view and routes

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ var express = require('express'),
 
 
 app.use(express.static('public'));
-
+app.use(require('cookie-parser')('c)378fHR37!mfVJ30vn28S938BMjrn'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
-app.use(express.static(__dirname + '/app'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
-res.sendFile(__dirname + '/index.html');
+  app.use(express.static(__dirname + '/app'));
+  app.use('/bower_components', express.static(__dirname + '/bower_components'));
+  res.cookie('cookie', 'COOKIE');
+  res.sendFile(__dirname + '/index.html');
+});
 
+app.get('/join', function(req, res) {
+  res.sendfile(__dirname + '/app/views/join.html');
+});
+
+app.post('/join', function(req, res) {
+  console.log("join form received");
+  res.redirect(303, '/');
 });
 
 // called when a client/user connects to the application via Socket.IO
