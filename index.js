b892f967d9086fd65f83b0a03b7a4ad60d7ee213
Michael Ashby <ashby.michael@gmail.com>
2016-02-06 11:42:17 -0800

    set cookie on submitting join form

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-var express = require('express'),
+var express    = require('express'),
 
-    app     = express(),                // create a new express application
-    http    = require('http').Server(app),  // create an http server with Node's HTTP module. // Pass it the express application
-    io      = require('socket.io')(http),   //Instantiates Socket.IO
-    cah     = require('./app/controllers/cah.server.js');
+    app        = express(),                // create a new express application
+    http       = require('http').Server(app),  // create an http server with Node's HTTP module. // Pass it the express application
+    io         = require('socket.io')(http),   //Instantiates Socket.IO
+    cah        = require('./app/controllers/cah.server.js'),
+    bodyParser = require('body-parser');
 
 
 app.use(express.static('public'));
 app.use(require('cookie-parser')('c)378fHR37!mfVJ30vn28S938BMjrn'));
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.text({ type: 'text/html' }));
+
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
   app.use(express.static(__dirname + '/app'));
   app.use('/bower_components', express.static(__dirname + '/bower_components'));
-  res.cookie('cookie', 'COOKIE');
+
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/join', function(req, res) {
-  res.sendfile(__dirname + '/app/views/join.html');
+  res.sendFile(__dirname + '/app/views/join.html');
 });
 
 app.post('/join', function(req, res) {
   console.log("join form received");
-  res.cookie('joined', 'true');
+  console.log(req.body);
+  res.cookie('name', req.body.name);
+  res.cookie('game', req.body.game);
+  // res.cookie('joined', 'true');
   res.redirect(303, '/');
 });
 
